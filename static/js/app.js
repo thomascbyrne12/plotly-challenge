fa792956dfea1b04db9445c93fac9ad4f144661e
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-01-11 21:14:38 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,54 @@
-d3.json("data/samples.json").then( d => {
-    var trace1 = {
-        ticks: d.otu_ids,
-        bars: d.sample_values,
-        text: d.otu_labels,
-        type: 'bar'
-    };
+function buildCharts(sample) {
 
-    data1 = [trace1];
+    d3.json(`/samples/${sample}`).then(d => {
+        var trace1 = {
+            ticks: d.otu_ids,
+            bars: d.sample_values,
+            text: d.otu_labels,
+            type: 'bar'
+        };
 
-    layout1 = {
-        bar: {
-            horizontal: true,
-        },
-        title: 'Horizontal Bar Chart'
-    };
+        data1 = [trace1];
 
-    Plotly.newPlot('bar', data1, layout1);
+        layout1 = {
+            bar: {
+                horizontal: true,
+            },
+            title: 'Horizontal Bar Chart'
+        };
 
-    var trace2 = {
-        x: d.out_ids,
-        y: d.samples_values,
-        marker: {
-            size: d.samples_values,
-            color: d.samples_values,
-            text: d.otu_labels
-        },
-        type: bubble
-    };
+        Plotly.newPlot('bar', data1, layout1);
 
-    data2 = [trace2];
+        var trace2 = {
+            x: d.out_ids,
+            y: d.samples_values,
+            marker: {
+                size: d.samples_values,
+                color: d.samples_values,
+                text: d.otu_labels
+            },
+            type: bubble
+        };
 
-    layout2 = {
-        title: 'Bubble Plot'
-    };
+        data2 = [trace2];
 
-    Plotly.newPlot('bubble', data2, layout2)
+        layout2 = {
+            title: 'Bubble Plot'
+        };
 
-    Document.write('sample-metadata', d.metadata[0]);
-});
+        Plotly.newPlot('bubble', data2, layout2)
+    });
+}
+
+function buildMetadata(sample) {
+
+    d3.json(`/samples/${metadata}`).then( d => {
+        var sample_metadata = d3.select("#sample-metadata");
+        sample_metadata.html("");
+
+        Object.entries(d).forEach(function ([key, value]) {
+            var paragraph = sample_metadata.append("p");
+            paragraph.text(`${key}: ${value}`);
+        });
+    });
+}
